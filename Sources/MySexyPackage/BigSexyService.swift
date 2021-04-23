a77be95a5d67dc77e0bddb0d3d9a9ac8e527b0ca
Alberto Gonzalez <jgonzalez@ia.com.mx>
2021-04-23 14:16:04 -0500

    Initial Commit

---
+++ Sources/MySexyPackage/BigSexyService.swift
@@ -0,0 +1,15 @@
+//
+//  BigSexyService.swift
+//  MyCurrentApp
+//
+//  Created by José Alberto González Gordillo on 23/04/21.
+//
+
+import Foundation
+
+public struct BigSexyService {
+
+    public static func doSomethingSpecial() {
+        print(" Something special happened ")
+    }
+}
